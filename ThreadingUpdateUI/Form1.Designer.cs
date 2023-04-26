51cf6baa0e78020d3a310ecc10d5fcc567bb73a4
Songpon Yonpan <59166402+SongponYonpan@users.noreply.github.com>
2023-04-26 12:17:23 -0300

    Completed Threading Demo (No thread / Multi thread)

--- ThreadingUpdateUI/Form1.Designer.cs
+++ ThreadingUpdateUI/Form1.Designer.cs
@@ -28,103 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnGo = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnGoNoThread = new System.Windows.Forms.Button();
-            this.lblCounter = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            btnGo = new Button();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnGoNoThread = new Button();
+            lblCounter = new Label();
+            SuspendLayout();
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(15, 153);
-            this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(312, 23);
-            this.btnGo.TabIndex = 0;
-            this.btnGo.Text = "Go";
-            this.btnGo.UseVisualStyleBackColor = true;
-            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            btnGo.Location = new Point(18, 177);
+            btnGo.Margin = new Padding(4, 3, 4, 3);
+            btnGo.Name = "btnGo";
+            btnGo.Size = new Size(364, 27);
+            btnGo.TabIndex = 0;
+            btnGo.Text = "Go";
+            btnGo.UseVisualStyleBackColor = true;
+            btnGo.Click += btnGo_Click;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(126, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 20);
-            this.textBox1.TabIndex = 2;
+            textBox1.Location = new Point(147, 14);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(237, 23);
+            textBox1.TabIndex = 2;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(15, 182);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(315, 402);
-            this.panel1.TabIndex = 3;
+            panel1.Location = new Point(18, 210);
+            panel1.Margin = new Padding(4, 3, 4, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(368, 464);
+            panel1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(126, 38);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(204, 20);
-            this.textBox2.TabIndex = 4;
+            textBox2.Location = new Point(147, 44);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(237, 23);
+            textBox2.TabIndex = 4;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Count to:";
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 17);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Count to:";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Sleep betwen count:";
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 47);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Sleep betwen count:";
             // 
             // btnGoNoThread
             // 
-            this.btnGoNoThread.Location = new System.Drawing.Point(12, 85);
-            this.btnGoNoThread.Name = "btnGoNoThread";
-            this.btnGoNoThread.Size = new System.Drawing.Size(312, 23);
-            this.btnGoNoThread.TabIndex = 7;
-            this.btnGoNoThread.Text = "Go No Thread";
-            this.btnGoNoThread.UseVisualStyleBackColor = true;
-            this.btnGoNoThread.Click += new System.EventHandler(this.btnGoNoThread_Click);
+            btnGoNoThread.Location = new Point(14, 98);
+            btnGoNoThread.Margin = new Padding(4, 3, 4, 3);
+            btnGoNoThread.Name = "btnGoNoThread";
+            btnGoNoThread.Size = new Size(364, 27);
+            btnGoNoThread.TabIndex = 7;
+            btnGoNoThread.Text = "Go No Thread";
+            btnGoNoThread.UseVisualStyleBackColor = true;
+            btnGoNoThread.Click += btnGoNoThread_Click;
             // 
             // lblCounter
             // 
-            this.lblCounter.AutoSize = true;
-            this.lblCounter.Location = new System.Drawing.Point(123, 111);
-            this.lblCounter.Name = "lblCounter";
-            this.lblCounter.Size = new System.Drawing.Size(85, 13);
-            this.lblCounter.TabIndex = 8;
-            this.lblCounter.Text = "[MY COUNTER]";
+            lblCounter.AutoSize = true;
+            lblCounter.Location = new Point(147, 128);
+            lblCounter.Margin = new Padding(4, 0, 4, 0);
+            lblCounter.Name = "lblCounter";
+            lblCounter.Size = new Size(89, 15);
+            lblCounter.TabIndex = 8;
+            lblCounter.Text = "[MY COUNTER]";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 596);
-            this.Controls.Add(this.lblCounter);
-            this.Controls.Add(this.btnGoNoThread);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnGo);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(394, 688);
+            Controls.Add(lblCounter);
+            Controls.Add(btnGoNoThread);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(panel1);
+            Controls.Add(textBox1);
+            Controls.Add(btnGo);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
